efa704add1643b643591efd8f09a06317088ab2c
duhoobo <alecdu@gmail.com>
2016-01-09 23:03:44 +0800

    Put example scripts in a subdirectory of `examples`

--- examples/protocols/durian.py
+++ examples/protocols/durian.py
@@ -3,35 +3,21 @@ from swarm.protocol import reply_parser_crlf
 
 def heartbeat(client):
     def make_request():
-        return "SS|tcp.heartbeat|7|uid=123\r\n"
-
-    client.send_for_reply(make_request(), reply_parser_crlf())
-
-
-def create_chatroom(client):
-    def make_request():
-        return "SS|chat.createroom|16|uid=123&roomid=7\r\n"
+        return "heartbeat\r\n"
 
     client.send_for_reply(make_request(), reply_parser_crlf())
 
 
 def enter_chatroom(client):
     def make_request():
-        return "SS|chat.enterroom|16|uid=123&roomid=7\r\n"
+        return "enter_chatroom\r\n"
 
     client.send_for_reply(make_request(), reply_parser_crlf())
 
 
 def leave_chatroom(client):
     def make_request():
-        return "SS|chat.outroom|16|uid=123&root=7\r\n"
-
-    client.send_for_reply(make_request(), reply_parser_crlf())
-
-
-def close_chatroom(client):
-    def make_request():
-        return "SS|chat.closeroom|16|uid=123&roomid=7\r\n"
+        return "leave_chatroom\r\n"
 
     client.send_for_reply(make_request(), reply_parser_crlf())
 
